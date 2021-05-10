7dedabe08c1d97750542b281d26ff0b6c8ac90fe
Nicky <51677808+Techgeek29@users.noreply.github.com>
2021-05-10 05:28:56 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const name ='nicky'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Nicky
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello From {name} </h1>
     </div>
   );
 }
